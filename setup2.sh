4fb83d39221fc02406273ce40f68c578d258ac84
gokuxofip <gokuxofip@10vpn.info>
2017-07-17 14:26:47 +0200

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install php7.0-zip
+configurazione php.ini
+sudo apt-get install composer
+sudo composer global require "laravel/installer" 
+sudo service apache2 restart
+cd /var/www/html
+echo "Type the project name: followed by [ENTER]"
+read ProjectName
+sudo ./.composer/vendor/bin/laravel new $ProjectName
