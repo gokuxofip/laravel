dc00ef7177766b1c47af65a9576539994d982951
gokuxofip <gokuxofip@10vpn.info>
2017-07-17 15:09:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,6 @@ configurazione php.ini
 sudo apt-get install composer
 sudo composer global require "laravel/installer" 
 sudo service apache2 restart
-cd /var/www/html
 echo "Type the project name: followed by [ENTER]"
 read ProjectName
-sudo ./home/user/.composer/vendor/bin/laravel new $ProjectName
+sudo ./.composer/vendor/bin/laravel new $ProjectName
